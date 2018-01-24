de9ab2976f8ec5c8cbf1c8f4b613b9016f3f3c5f
Peter Krevenets <peter.k.nets@gmail.com>
2018-01-24 09:24:06 +0200

    Handle '/' route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require("path");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
 app.listen(3000, () => {
   console.log("Server started on port 3000...");
 });
